6ef2fefafc6946c734ee10febe3d34f1126ab64a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 06:31:32 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Hide'] = 'הסתר';
+$lang['See more'] = 'ראה עוד';
+$lang['show tag groups'] = 'הצג קבוצות תגיות';
