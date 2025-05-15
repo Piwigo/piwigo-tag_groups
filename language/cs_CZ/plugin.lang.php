ca2399f91ea674ed146b41c5f71f5646dcba0500
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-05-15 13:45:38 +0200

    Update cs_CZ, thanks to MiPi

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -20,4 +20,6 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['show tag groups'] = 'Zobrazit skupiny štítků';
+$lang['show tag groups'] = 'Zobrazit skupiny tagů';
+$lang['See more'] = 'Zobrazit více';
+$lang['Hide'] = 'Schovat';
