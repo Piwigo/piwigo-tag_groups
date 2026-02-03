5e9b119c392f21e4089defbd26b9dfd3bc1758a5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-03 19:37:19 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Hide'] = '숨기기';
+$lang['See more'] = '더 보기';
+$lang['show tag groups'] = '태그 그룹 표시';
