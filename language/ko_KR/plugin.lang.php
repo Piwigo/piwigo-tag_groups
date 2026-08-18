a1db4e259c3f142d48c036f4b432432614ae5301
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-08-18 07:00:41 +0200

    Update ko_KR, thanks to yjchoi

--- language/ko_KR/plugin.lang.php
+++ language/ko_KR/plugin.lang.php
@@ -8,3 +8,6 @@
 $lang['Hide'] = '숨기기';
 $lang['See more'] = '더 보기';
 $lang['show tag groups'] = '태그 그룹 표시';
+$lang['General'] = '일반';
+$lang['Show group filters on the tags page'] = '태그 페이지에 그룹 필터 표시';
+$lang['Show tag groups as separate fields'] = '태그 그룹을 별도의 필드로 표시';
