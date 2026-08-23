3a4d862a4ef3df940e4162326f3b3e3d75f8b5ea
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-08-23 07:09:09 +0200

    Update fr_CA, thanks to bgoutier

--- language/fr_CA/plugin.lang.php
+++ language/fr_CA/plugin.lang.php
@@ -6,3 +6,5 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['show tag groups'] = 'montrer les mots-clés par groupes';
+$lang['Show group filters on the tags page'] = 'Afficher les filtres par groupe sur la page des mots-clés';
+$lang['Show tag groups as separate fields'] = 'Afficher les groupes de mots-clés comme des champs distincts';
