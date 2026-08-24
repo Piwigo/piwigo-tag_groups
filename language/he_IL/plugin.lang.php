39e215b9fe51e555c02142bbe3830a3808cb4092
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-08-24 07:11:26 +0200

    Update he_IL, thanks to epifeny

--- language/he_IL/plugin.lang.php
+++ language/he_IL/plugin.lang.php
@@ -8,3 +8,6 @@
 $lang['Hide'] = 'הסתר';
 $lang['See more'] = 'ראה עוד';
 $lang['show tag groups'] = 'הצג קבוצות תגיות';
+$lang['General'] = 'כללי';
+$lang['Show group filters on the tags page'] = 'הצג מסנני קבוצה בעמוד התגיות';
+$lang['Show tag groups as separate fields'] = 'הצג קבוצות תגיות כשדות נפרדים';
